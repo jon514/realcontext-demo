fb0f50a47d963ad8b8bf3f00df66b14cc7dadcfb
jon514 <jonathan.hiltser@gmail.com>
2014-02-09 16:38:56 -0500

    blah

--- app/code/local/Realcontext/Recommendations/Block/Recommendations.php
+++ app/code/local/Realcontext/Recommendations/Block/Recommendations.php
@@ -1,6 +1,6 @@
 <?php
 
-class Realcontext_Recommendations_Block_Recommendations extends Mage_Core_Block_Template {
+class Realcontext_Recommendations_Block_Recommendations extends Mage_Catalog_Block_Product_Abstract {
 	public function getRecommendations() {
 		$id = rand(0,1);
 		return Mage::getModel('realcontext/recommendations')->getRecommendations($id);

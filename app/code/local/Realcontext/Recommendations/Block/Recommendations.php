367f709ee4c2240c6e111d0be2eb20c8fb879e01
jon514 <jonathan.hiltser@gmail.com>
2014-02-09 15:27:53 -0500

    blah

--- app/code/local/Realcontext/Recommendations/Block/Recommendations.php
+++ app/code/local/Realcontext/Recommendations/Block/Recommendations.php
@@ -2,7 +2,7 @@
 
 class Realcontext_Recommendations_Block_Recommendations extends Mage_Core_Block_Template {
 	public function getRecommendations() {
-		$id = $this->getRequest('id');
+		$id = $this->getRequest()->getParam('id');
 		Mage::getModel('realcontext/recommendations')->getRecommendations($id);
 	}
 }

055f5c12c5c821758b15964348589741aeaac196
jon514 <jonathan.hiltser@gmail.com>
2014-02-09 00:18:35 -0500

    First commit RealContext module

---
+++ app/code/local/RealContext/controllers/IndexController.php
@@ -0,0 +1,23 @@
+<?php
+/**
+ *
+ * Piwik Extension for Magento created by Adrian Speyer
+ * Get Piwik at http://www.piwik.org - Open source web analytics
+ *
+ * @category    Mage
+ * @package     Mage_PiwikAnalytics_Controller_IndexController
+ * @copyright   Copyright (c) 2012 Adrian Speyer. (http://www.adrianspeyer.com)
+ * @license http://www.gnu.org/licenses/gpl-3.0.html GPL v3 or later
+ *
+ */
+
+class RealContext_IndexController extends Mage_Core_Controller_Front_Action
+{
+    public function indexAction()
+    {
+
+    	$this->loadLayout();
+		$this->renderLayout();
+	
+    }
+}
